e020bacc786fa661cfb6dd9d3f20a396dbf14ba9
KISAAKYE HENRY RUKUNDO <48773051+rukundohenry2@users.noreply.github.com>
2022-12-10 05:16:31 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="media/logo/logo.png">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Enviro Lab</title>
+</head>
+<body>
+    
+    <div class="middlepart1">
+        <?php include "header.html" ?>
+        <div class="aboutsection">
+            <div class="aboutdesc">
+                <h3>What we are.</h3>
+                <p>Offered say visited elderly and. Waited period are played family man formed. He ye body or made on pain part meet. You one delay nor begin our folly abode. By disposed replying mr me unpacked no. As moonlight of my resolving unwilling.</p>
+                <a href="">More</a>
+            </div>
+            <img src="media/vector/imgcollection.png" alt="">
+        </div>
+    </div>
+    <div class="middlepart2">
+        <div class="conservesection">
+            <img src="media/vector/gallery.png" alt="">
+            <div class="conservedesc">
+                <h3>Environmental conservation</h3>
+                <p>Offered say visited elderly and. Waited period are played family man formed. He ye body or made on pain part meet. You one delay nor begin our folly abode. By disposed replying mr me unpacked no. As moonlight of my resolving unwilling.</p>
+                <!-- <a href="">More</a> -->
+            </div>
+        </div>
+    </div>
+    
+
+</body>
+</html>
