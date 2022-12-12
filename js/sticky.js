d98a485b9ad8d6d49130e3976ce73b10f7cbee5e
KISAAKYE HENRY RUKUNDO <48773051+rukundohenry2@users.noreply.github.com>
2022-12-13 00:02:44 +0300

    fixed a bunch of staff

--- js/sticky.js
+++ js/sticky.js
@@ -1,29 +1,45 @@
-// // When the user scrolls the page, execute myFunction
-// window.onscroll = function() {myFunction()};
+// Get the navbar
+var navbar = document.getElementById("socialmenu");
 
-// // Get the navbar
-// var navbar = document.getElementsByClassName("menuitems");
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+console.log(sticky)
 
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
+var targetWidth= 576
 
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-function windowwidth(){
-    var targetWidth = $(window).width()
-    console.log(targetWidth)
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    if ( document.body.clientWidth >=  targetWidth ) {     
+        //Add your javascript for screens wider than or equal to 768 here
+        
+        document.querySelector('#menustick').classList.add('sticky')
+        $('.blankdiv .logo3').css('display','block')
+        $('.menubar .logo1').css('display','none')
+        //$('.menubar .menusocial .blankdiv').css('display','none')
+        $('.middlepart1').css('height','fit-content')
+
+
+        console.log("henry in the building")
+
+    }
+    
+    
+  } else {
+    if ( document.body.clientWidth >=  targetWidth ) { 
+        document.querySelector('#menustick').classList.remove('sticky')
+        console.log("henry is really out of the building")
+        $('.menubar .logo3').css('display','none')
+        $('.menubar .logo1').css('display','block')
+        $('.middlepart1').css('height','100vh')
+        //$('.menubar .menusocial .blankdiv').css('display','block')
+        //$('.menubar').css('flex-direction','column')
+    }
+  }
 }
 
 
-// if ( $(window).width() &gt;= targetWidth) {     
-//   //Add your javascript for screens wider than or equal to 768 here
-// }
-// else {
-//   //Add your javascript for screens smaller than 768 here
-// }
+
